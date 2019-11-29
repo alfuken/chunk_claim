1d90507d4b3cc2676b7980605dd7aec1e8f7c88f
Bohdan Shchepanskyy <bs@autoplenum.de>
2019-11-29 12:10:52 +0100

    fix typo, bump version

--- src/main/java/lime/chunk_claim/EventHandlers.java
+++ src/main/java/lime/chunk_claim/EventHandlers.java
@@ -16,6 +16,7 @@ import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Mod.EventBusSubscriber
